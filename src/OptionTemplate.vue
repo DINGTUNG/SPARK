cad6e19b2523c090093f1ef661d95bcaf0ac98c5
MiMi <janicehsu890228@gmail.com>
2023-07-08 23:12:03 +0800

    OptionTemplate模板done!!

---
+++ src/OptionTemplate.vue
@@ -0,0 +1,20 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  methods: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>

c0f768e38dc3673b89ed854a24fc3504c4be8701
DINGTUNG <jjwelkintung0124@gmail.com>
2023-07-12 21:10:48 +0800

    捐款內容

--- src/views/pages/donate/Donate.vue
+++ src/views/pages/donate/Donate.vue
@@ -4,11 +4,27 @@ import DonateContentCard from '../../../components/donate/DonateContentCard.vue'
 </script>
 
 <template>
-  <div>
-    <DonateContentCard />
+  <div class="body">
+
+    <div class="section_banner">
+      <img src="" alt="">
+    </div>
+    <div class="section_donate">
+      <div class="donate_container">
+        <h1 class="title">捐款內容</h1>
+        <div class="title_line"></div>
+      </div>
+    </div>
+
+    <div>
+      <DonateContentCard />
+    </div>
+
   </div>
 </template>
 
 
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+@import "@/assets/sass/pages/donate";
+</style>

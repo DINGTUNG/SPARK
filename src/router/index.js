a08ada55811edbb719276d897d9cf30740e4d762
yiiijie <qaq2104602@gmail.com>
2023-07-12 16:24:31 +0800

    解衝突

--- src/router/index.js
+++ src/router/index.js
@@ -12,15 +12,7 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     },
-    {
-      path: '/about',
-      name: 'about'
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-    }
-      component: Home
-    },
+  
     {
       path: '/login',
       name: 'login',

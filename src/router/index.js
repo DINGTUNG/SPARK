41a0de43f8dc20dc17873e9a176d546d9496888c
lockon1248 <tom5678tom5678t@yahoo.com.tw>
2023-07-12 18:14:51 +0800

    no message

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/pages/Home.vue'
 import Donate from '../views/pages/Donate.vue'
@@ -10,21 +9,6 @@ const router = createRouter({
   routes: [
     {
 
-=======
-import {
-  createRouter,
-  createWebHistory
-} from 'vue-router'
-import Home from '../views/pages/home/Home.vue'
-import Login from '../views/pages/login/Login.vue'
-import Donate from '../views/pages/donate/Donate.vue'
-
-
-const router = createRouter({
-  history: createWebHistory(
-    import.meta.env.BASE_URL),
-  routes: [{
->>>>>>> bb1e086b6fa7bb50880d1b18ef84718e4c07d4ac
       path: '/',
       name: 'home',
       component: Home
@@ -39,16 +23,7 @@ const router = createRouter({
       path: '/donate',
       name: 'donate',
       component: Donate
-<<<<<<< HEAD
-    },
-    {
-      path: '/news',
-      name: 'news',
-      component: News
-    },
-=======
     }
->>>>>>> bb1e086b6fa7bb50880d1b18ef84718e4c07d4ac
   ]
 })
 

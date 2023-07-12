3a7141f96f076073f3f8ed12ebfcc3171bff516d
MiMi <janicehsu890228@gmail.com>
2023-07-12 21:07:56 +0800

    更

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,34 @@
 import { createRouter, createWebHistory } from 'vue-router'
+// import HomeView from '../views/HomeView.vue'
+import SponsorLocation from '../views/pages/sponsor/SponsorLocation.vue'
 import Home from '../views/pages/home/Home.vue'
 import Donate from '../views/pages/donate/Donate.vue'
 import Login from '../views/pages/login/Login.vue'
-import News from '../views/pages/News.vue'
+
 
 const router = createRouter({
-      
+  history: createWebHistory(
+    import.meta.env.BASE_URL),
+  routes: [{
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/sponsor-location',
+    name: 'sponsor-location',
+    component: SponsorLocation
+  },
+  {
+    path: '/donate',
+    name: 'donate',
+    component: Donate
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  }
   ]
 })
 

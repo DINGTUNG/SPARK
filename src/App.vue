d4322363198cc43b49fdecdca15ee54e6e399009
njs-kayla <kayla881221@gmail.com>
2023-07-13 14:54:04 +0800

    路徑修改 & 解衝突

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,7 @@ import {  } from 'vue-router'
 
 import Header from '@/layout/Header.vue';
 import Footer from '@/layout/Footer.vue';
-<<<<<<< HEAD
 import RightMenu from '@/layout/RightMenu.vue';
-=======
-<<<<<<< HEAD
-import RightMenu from '@/layout/RightMenu.vue';
-=======
-import RightMenu from '@/assets/layout/RightMenu.vue';
->>>>>>> e1a874cb6ee2dcd3b478cc5d4346480144b734ce
->>>>>>> 6be306431fbf4520ddfac9c5e9edcd8e429283ea
 
 
 
@@ -20,29 +12,10 @@ import RightMenu from '@/assets/layout/RightMenu.vue';
 
 
 <template>
-<<<<<<< HEAD
-  <Header />
-=======
-<<<<<<< HEAD
-  <!-- <Header/> -->
-
-  <RightMenu />
-
-
-  <Footer/>
-=======
   <Header/>
->>>>>>> 6be306431fbf4520ddfac9c5e9edcd8e429283ea
   <RouterLink to="/single-news" class="link single-news">最新消息</RouterLink>
-
   <RightMenu />
-
-
   <Footer />
->>>>>>> e1a874cb6ee2dcd3b478cc5d4346480144b734ce
-
-
-
 </template>
 
 <style scoped>

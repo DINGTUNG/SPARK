13990914792702c2d39c8986d9ecc916649d34e6
MiMi <janicehsu890228@gmail.com>
2023-07-14 04:12:09 +0800

    修改右導覽列路徑

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@ import { RouterLink, RouterView } from 'vue-router'
 import Header from '@/layout/Header.vue';
 import Footer from '@/layout/Footer.vue';
 import LeftMenu from '@/layout/LeftMenu.vue';
-import RightMenu from '@/layout/LeftMenu.vue';
+import RightMenu from '@/layout/RightMenu.vue';
 
 </script>
 
@@ -21,7 +21,7 @@ import RightMenu from '@/layout/LeftMenu.vue';
   <LeftMenu />
   <RouterLink to="/single-news" class="link single-news">最新消息，首頁還沒完成前不要刪</RouterLink><br>
   <RouterLink to="/sponsor" class="link single-news">我要認養，側邊導覽列沒修好不能刪，不然會看不到</RouterLink>
-  
+
   <RouterView />
   <Footer />
 </template>

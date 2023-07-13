3e3709de754691f453a4eea4f600787a8ecd6be9
MiMi <janicehsu890228@gmail.com>
2023-07-14 04:01:53 +0800

    更新app.vue

--- src/App.vue
+++ src/App.vue
@@ -16,11 +16,13 @@ import RightMenu from '@/layout/LeftMenu.vue';
 
 <template>
   <Header />
-  <!-- <RouterView/>  -->
+
   <RightMenu />
+  <LeftMenu/>
   <RouterLink to="/single-news" class="link single-news">最新消息，首頁還沒完成前不要刪</RouterLink><br>
   <RouterLink to="/sponsor" class="link single-news">我要認養，側邊導覽列沒修好不能刪，不然會看不到</RouterLink>
-<Footer />
+  <RouterView />
+  <Footer />
 </template>
 
 <style scoped>

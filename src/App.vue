8bccde1d7abafef1adfdc4e69675e14e3e98993f
dos8742 <zxc897004@gmail.com>
2023-07-13 21:39:40 +0800

    更新LeftMenu

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,15 @@
 <script setup>
 
-import { } from 'vue-router'
-
-import { provide } from 'vue'
-import getImgSrc from './utils/getImgSrc.js'
-
-provide('getImgSrc', getImgSrc)
+import {  } from 'vue-router'
 
 import Header from '@/layout/Header.vue';
 import Footer from '@/layout/Footer.vue';
-<<<<<<< HEAD
 import LeftMenu from '@/layout/LeftMenu.vue';
-=======
-import RightMenu from '@/layout/RightMenu.vue';
-
-
->>>>>>> 2234a40e13b1f8f6e02223834c920454e010b8a8
 
 </script>
 
 
 <template>
-<<<<<<< HEAD
   <!-- <Header/> -->
   <LeftMenu/>
   <!-- <Footer/> -->
@@ -34,34 +22,16 @@ import RightMenu from '@/layout/RightMenu.vue';
 
 
 
-=======
-<<<<<<< HEAD
-  <Header/> 
-=======
-  <Header />
->>>>>>> dev
-
-  <RightMenu />
-  <RouterLink to="/single-news" class="link single-news">最新消息</RouterLink>
-
-<<<<<<< HEAD
-  <Footer/>
- 
-  <!-- <RouterView /> -->
-
-=======
-  <RightMenu />
-  <Footer />
->>>>>>> dev
->>>>>>> 2234a40e13b1f8f6e02223834c920454e010b8a8
 </template>
 
 <style scoped>
 @import '@/assets/sass/base/fonts/_fonts.scss';
 
 
-body {
+body{
   margin: 0;
   padding: 0;
 }
+
+
 </style>

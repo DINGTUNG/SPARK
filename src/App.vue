1d8f7ac2faaa48f68c8d5f12b8847a5b13918182
MiMi <janicehsu890228@gmail.com>
2023-07-14 04:13:21 +0800

    更新app.vue 中router link的順序讓連結置頂

--- src/App.vue
+++ src/App.vue
@@ -17,10 +17,12 @@ import RightMenu from '@/layout/RightMenu.vue';
 <template>
   <Header />
 
-  <RightMenu />
-  <LeftMenu />
   <RouterLink to="/single-news" class="link single-news">最新消息，首頁還沒完成前不要刪</RouterLink><br>
   <RouterLink to="/sponsor" class="link single-news">我要認養，側邊導覽列沒修好不能刪，不然會看不到</RouterLink>
+
+
+  <RightMenu />
+  <LeftMenu />
 
   <RouterView />
   <Footer />

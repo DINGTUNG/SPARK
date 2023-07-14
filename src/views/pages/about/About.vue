d2b7bd1fb3a0abaf1d7212bec4a0d8cc706eaa93
MiMi <janicehsu890228@gmail.com>
2023-07-14 23:36:48 +0800

    修改about.vue並註解變數以順利run build

--- src/views/pages/about/About.vue
+++ src/views/pages/about/About.vue
@@ -1,12 +1,12 @@
 <script setup>
 
-import { ref } from 'vue';
-const h1 = ref("認識星火")
+// import { ref } from 'vue';
+// const h1 = ref("認識星火")
 
 </script>
 
 <template>
-  <div class="about_countainer">
+  <div class="about_container">
     <div class="main_body">
       <div class="about_banner">
         <img :src="'assets/images/about/banner.jpg'" alt="認識星火封面">
@@ -18,18 +18,6 @@ const h1 = ref("認識星火")
       
 
     </div>
-  </div>
-
-
-
-
-
-
-
-
-
-
-
   </div>
 </template>
 

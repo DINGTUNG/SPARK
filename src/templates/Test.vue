f0b9e75d98b7929de62cdafbc166ce0f2ffdf21a
MiMi <janicehsu890228@gmail.com>
2023-07-14 23:06:37 +0800

    修改按鈕scss設定並增加hover、active效果

--- src/templates/Test.vue
+++ src/templates/Test.vue
@@ -4,17 +4,17 @@
 
 <template>
   <div>
+    <RouterLink to="/test" class="link test">測試頁面</RouterLink>
 
-<button class=".test_btn">呼叫狸貓</button>
+    <button class="test_btn">呼叫狸貓</button>
   </div>
 </template>
 
 <style scoped lang="scss">
 @import '@/assets/sass/components/func-items/buttons/_buttons-mixin';
 
-.test_btn{
-@include regularBlueBtn_MB
+button.test_btn {
+  @include smallGoldBtn_PC;
 
 }
-
 </style>

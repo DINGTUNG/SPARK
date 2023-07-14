4e00ec4f4d5c97239032682090bb407ad3b47aec
yiiijie <qaq2104602@gmail.com>
2023-07-14 21:14:08 +0800

    新增"聯絡星火"內容（製作中）

--- src/views/pages/about/About.vue
+++ src/views/pages/about/About.vue
@@ -1,6 +1,23 @@
+<script setup>
+
+import { ref } from 'vue';
+const h1 = ref("認識星火")
+
+</script>
+
 <template>
-  <div class="banner">
-    <img src="@/assets/images/about/banner.jpg" alt="about_banner">
+  <div class="about_countainer">
+    <div class="main_body">
+      <div class="about_banner">
+        <img :src="'assets/images/about/banner.jpg'" alt="認識星火封面">
+      </div>
+      <div class="title_block">
+        <h1>認識星火</h1>
+        <img class="deco" :src="'assets/decorations/illustration/decorative_line.png'" alt="裝飾線">
+      </div>
+      
+
+    </div>
   </div>
 
 
@@ -12,4 +29,11 @@
 
 
 
+
+  </div>
 </template>
+
+
+<style scoped lang="scss">
+@import "@/assets/sass/pages/about";
+</style>

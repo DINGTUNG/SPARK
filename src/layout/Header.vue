cb8c2e07b809fa43e9ac53a6dacbbfde2eee8c6d
MiMi <janicehsu890228@gmail.com>
2023-07-15 15:42:55 +0800

    修改work-result router name 並新增全域sass

--- src/layout/Header.vue
+++ src/layout/Header.vue
@@ -10,7 +10,7 @@ import { RouterLink } from 'vue-router'
             <nav>
                 <RouterLink to="/about" class="link about">認識星火</RouterLink>
                 <RouterLink to="/service" class="link service">服務內容</RouterLink>
-                <RouterLink to="/result" class="link result">成果佈告欄</RouterLink>
+                <RouterLink to="/work-result" class="link work-result">成果佈告欄</RouterLink>
                 <RouterLink to="/sponsor-location" class="link sponsor-location">認養計畫</RouterLink>
                 <RouterLink to="/donate" class="link donate">捐款專案</RouterLink>
                 <RouterLink to="/contact" class="link contact">星火之友</RouterLink>

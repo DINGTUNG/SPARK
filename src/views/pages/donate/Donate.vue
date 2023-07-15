e40b4dd3902b667fdb8f408fb4174e102f1a6e38
DINGTUNG <jjwelkintung0124@gmail.com>
2023-07-15 16:54:34 +0800

    捐款內容調整

--- src/views/pages/donate/Donate.vue
+++ src/views/pages/donate/Donate.vue
@@ -6,26 +6,22 @@ const h1 = ref("捐款內容")
 </script>
 
 <template>
-  <div class="body">
+  <div class="banner">
+    <img :src="'assets/images/donate/donate-content/banner.jpg'" alt="捐款內容封面">
+  </div>
+  <div class="donate_container">
+    <div class="main_body">
+      <div class="title_wrap">
+        <h1 class="title">{{ h1 }}</h1>
+        <div class="titleline">
 
-    <div class="section_banner">
-      <img :src="'assets/images/donate/donate-content/banner.jpg'" alt="捐款內容封面">
-    </div>
-    <div class="section_donate">
-      <div class="donate_content">
-        <div class="title_wrap">
-          <h1 class="title">{{ h1 }}</h1>
-          <div class="titleline"></div>
         </div>
-        <DonateContentCard />
       </div>
-      <div class="deco_flowering">
-        <img :src="'assets/characters/boy/boy_flowering.png'" alt="男孩澆花">
-      </div>
+      <DonateContentCard />
     </div>
-
-
-
+    <div class="deco_flowering">
+      <img :src="'assets/characters/boy/boy_flowering.png'" alt="男孩澆花">
+    </div>
   </div>
 </template>
 

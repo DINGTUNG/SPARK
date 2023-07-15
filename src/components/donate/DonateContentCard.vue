f458dd1cc8f62982552cc811a80370a27474b15d
DINGTUNG <jjwelkintung0124@gmail.com>
2023-07-16 01:07:15 +0800

    修改 donate.scss 和 DonateContentCard.vue 的 RWD (R一半)

--- src/components/donate/DonateContentCard.vue
+++ src/components/donate/DonateContentCard.vue
@@ -59,7 +59,9 @@ const donateContentCardList = reactive([
   <div class="donate_content_card_list">
 
     <div class="donate_content_card" v-for="donateContentCard in donateContentCardList" :key="donateContentCard.id">
-      <img :src="donateContentCard.imgSrc" :alt="donateContentCard.title">
+      <div class="card_pic">
+        <img :src="donateContentCard.imgSrc" :alt="donateContentCard.title">
+      </div>
       <div class="card_content">
         <h3>{{ donateContentCard.title }}</h3>
         <p>{{ donateContentCard.content }}</p>
@@ -77,53 +79,123 @@ const donateContentCardList = reactive([
 div.donate_content_card_list {
   display: flex;
   flex-wrap: wrap;
-  justify-content: space-between;
-  gap: 45px 0;
+
+  @include custom-responsive("xs sm") {
+    gap: 6vw 0;
+  }
+
+  @include custom-responsive("xl xxl") {
+    justify-content: space-between;
+    gap: 3.5vw 0;
+  }
 
   div.donate_content_card {
-    width: 46%;
     background-color: #fff;
     display: flex;
     flex-direction: column;
-    border-radius: $br_PC;
-    @include boxShadow_PC;
 
-    img {
+    @include custom-responsive("xs sm") {
       width: 100%;
-      border-radius: $br_PC $br_PC 0 0;
+      @include boxShadow_MB;
+      border-radius: $br_MB;
+    }
+
+    @include custom-responsive("xl xxl") {
+      width: 44%;
+      @include boxShadow_PC;
+      border-radius: $br_PC;
+
+      &:hover img {
+        transform: scale(1.1);
+        opacity: 0.8;
+      }
+    }
+
+    div.card_pic {
+      overflow: hidden;
+
+      @include custom-responsive("xs sm") {
+        border-radius: $br_MB $br_MB 0 0;
+      }
+
+      @include custom-responsive("xl xxl") {
+        border-radius: $br_PC $br_PC 0 0;
+      }
+
+      img {
+        width: 100%;
+        transition: 0.4s;
+      }
     }
 
     div.card_content {
       box-sizing: border-box;
-      padding: 40px 45px;
       display: flex;
       flex-direction: column;
       flex-grow: 1;
 
+      @include custom-responsive(" xs sm") {
+        padding: 4vw 4.5vw;
+      }
+
+      @include custom-responsive(" xl xxl") {
+        padding: 2vw 2.3vw;
+      }
+
       h3 {
-        @include h3_PC;
         color: $primaryBrandBlue;
-        padding-bottom: 15px;
+
+        @include custom-responsive(" xs sm") {
+          @include h3_MB;
+          padding-bottom: 2vw;
+        }
+
+        @include custom-responsive(" xl xxl") {
+          @include h3_PC;
+          padding-bottom: 0.8vw;
+        }
       }
 
       p {
-        @include h6_PC;
         color: $basicFontColor;
-        padding-bottom: 40px;
+
+        @include custom-responsive(" xs sm") {
+          @include h6_MB;
+          padding-bottom: 4vw;
+        }
+
+        @include custom-responsive(" xl xxl") {
+          @include h6_PC;
+          padding-bottom: 2vw;
+        }
       }
 
       h4 {
-        @include h4_PC;
         color: $secondaryLightBlue2;
         margin-top: auto;
-        padding-bottom: 40px;
+
+        @include custom-responsive(" xs sm") {
+          @include h4_MB;
+          padding-bottom: 4vw;
+        }
+
+        @include custom-responsive(" xl xxl") {
+          @include h4_PC;
+          padding-bottom: 2vw;
+        }
       }
 
       button {
-        @include smallBlueBtn_PC;
         border: none;
         margin-left: auto;
 
+        @include custom-responsive(" xs sm") {
+          @include smallBlueBtn_MB;
+        }
+
+        @include custom-responsive(" xl xxl") {
+          @include smallBlueBtn_PC;
+        }
       }
 
     }

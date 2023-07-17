6c08fc3a54a1446d7730d0886c8efab1aacae93f
MiMi <janicehsu890228@gmail.com>
2023-07-18 01:26:00 +0800

    進行CheckOutCards到一半，存檔

--- src/components/cards/CheckOutCards.vue
+++ src/components/cards/CheckOutCards.vue
@@ -1,15 +1,32 @@
 <script setup>
 import { useRoute } from 'vue-router';
-import { ref, reactive, watch } from 'vue';
+import { ref, reactive, watch,computed } from 'vue';
 
 const checkOutOverviewTitle = ref("");
-const checkOutData = reactive({
-
+const checkOutDataList = reactive([
+{
   sponsorLocation: "台北",
   sponsorCount: 1,
   sponsorPrice: 2000,
+},
+{
+  sponsorLocation: "台中",
+  sponsorCount: 1,
+  sponsorPrice: 2000,
+},
+{
+  sponsorLocation: "台南",
+  sponsorCount: 1,
+  sponsorPrice: 2000,
+},
+{
+  sponsorLocation: "台東",
+  sponsorCount: 1,
+  sponsorPrice: 2000,
+},
+])
 
-})
+const totalPrice = computed( () => checkOutDataList.reduce((total, checkOutData) => total + checkOutData.sponsorPrice, 0))
 
 const paymentTitle = ref("");
 
@@ -17,11 +34,11 @@ const currentRoute = useRoute();
 console.log(currentRoute.name);
 
 watch(() => {
-  if (currentRoute.name === "sponsor-checkout-step-1" || currentRoute.name === "sponsor-checkout-step-2" || currentRoute.name  === "sponsor-checkout-step-3") {
+  if (currentRoute.name === "sponsor-checkout-step-1" || currentRoute.name === "sponsor-checkout-step-2" || currentRoute.name === "sponsor-checkout-step-3") {
     checkOutOverviewTitle.value = "認養總覽";
     paymentTitle.value = "認養費(月)";
 
-  } else if (currentRoute.name === "donate-checkout-step-1" || currentRoute.name === "donate-checkout-step-2")  {
+  } else if (currentRoute.name === "donate-checkout-step-1" || currentRoute.name === "donate-checkout-step-2") {
     checkOutOverviewTitle.value = "捐款總覽";
     paymentTitle.value = "總計";
 
@@ -36,16 +53,23 @@ watch(() => {
 <template>
   <div class="check_out_card">
 
-    <h2>{{ checkOutOverviewTitle }}</h2>
+    <h2 class="check_out_overview_title">{{ checkOutOverviewTitle }}</h2>
 
-    <div class="check_out_data">
-      <span>{{ checkOutData.sponsorLocation }}地區</span>
-      <span>共 {{ checkOutData.sponsorCount }} 位</span>
-      <span>NTD {{ checkOutData.sponsorPrice }}</span>
+    
+    <div class="checkout_datalist">
+      <div v-for="checkOutData in checkOutDataList" :key="checkOutData.sponsorLocation" class="check_out_data">
+        <span>{{ checkOutData.sponsorLocation }}地區</span>
+        <span>共 {{ checkOutData.sponsorCount }} 位</span>
+        <span>NTD {{ checkOutData.sponsorPrice }}</span>
+      </div>
     </div>
 
-    <h4>{{ paymentTitle }}</h4>
-    <h3>NTD 6000</h3>
+    <div class="payment">
+      <div class="payment_info">
+      <span class="payment_title">{{ paymentTitle }}</span>
+      <span  class="total_price">{{totalPrice}}</span>
+    </div>
+    </div>
 
   </div>
 </template>
@@ -53,26 +77,5 @@ watch(() => {
 <style scoped lang="scss">
 @import '@/assets/sass/components/func-items/cards/checkout-cards';
 
-div.check_out_card {
-  width: 500px;
-  height: 400px;
-  @include boxShadow_PC;
-  border-radius: $br_PC;
 
-  div.check_out_data {
-    display: flex;
-    justify-content: space-evenly;
-    gap: 20px;
-
-
-    span {
-      @include h5_PC
-    }
-
-  }
-
-
-
-
-}
 </style>

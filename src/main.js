5cb8f385fd84405eddd72689aa367d77f88da876
MiMi <janicehsu890228@gmail.com>
2023-07-18 20:56:01 +0800

    導入pinia完成認養購物車!

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { createPinia } from 'pinia'
 
-// import '@/assets/sass/style.scss';
 
+const pinia = createPinia()
 const app = createApp(App)
 
 app.use(router)
 
+app.use(pinia)
 app.mount('#app')
 

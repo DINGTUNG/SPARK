70791a4bca8822818927e167b944a5383f75de34
yiiijie <qaq2104602@gmail.com>
2023-07-20 14:11:49 +0800

    測試 會員中心 element ui顏色修改

--- src/views/pages/member-center/MemberCenter.vue
+++ src/views/pages/member-center/MemberCenter.vue
@@ -20,23 +20,47 @@
       name: 'Tom',
       address: 'No. 189, Grove St, Los Angeles',
     },
+    {
+      date: '2016-05-03',
+      name: 'Tom',
+      address: 'No. 189, Grove St, Los Angeles',
+    },
+    {
+      date: '2016-05-02',
+      name: 'Tom',
+      address: 'No. 189, Grove St, Los Angeles',
+    },
+    {
+      date: '2016-05-04',
+      name: 'Tom',
+      address: 'No. 189, Grove St, Los Angeles',
+    },
+    {
+      date: '2016-05-01',
+      name: 'Tom',
+      address: 'No. 189, Grove St, Los Angeles',
+    },
   ]
   </script>
 
 
 <template>
 
-
-
-<br v-for="n in 100" :key="n">
-    <el-table :data="tableData" border style="width: 100%">
+<!-- <br v-for="n in 30" :key="n"> -->
+   <el-table :data="tableData" border style="width: 100%" :header-cell-style="{ backgroundColor: '#ffe874' }" :color="'#dd3c3c'">
       <el-table-column prop="date" label="Date" width="180" />
       <el-table-column prop="name" label="Name" width="180" />
       <el-table-column prop="address" label="Address" />
     </el-table>
   </template>
 
+
 <style scoped lang="scss">
 @import "@/assets/sass/pages/member-center/_member-center";
+.el-table{
+    color:$primaryBrandBlue;
+};
+
+
 </style>
   

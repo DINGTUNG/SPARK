96fef2fea7174d0b1962fc60c24e35454a7eacc7
yiiijie <qaq2104602@gmail.com>
2023-07-20 15:30:04 +0800

    測試 element ui顏色修改（失敗中）

--- src/views/pages/member-center/MemberCenter.vue
+++ src/views/pages/member-center/MemberCenter.vue
@@ -41,41 +41,52 @@
       address: 'No. 189, Grove St, Los Angeles',
     },
   ]
+
+
+  const letterRow = ({ row, rowIndex }) => {
+    if(rowIndex % 2 == 0){
+      return "test-odd test-blue";
+    } else {
+      return "test-even test-red";
+    }
+  };
+
   </script>
 
 
 <template>
 
+<div class="about_container">
+  <div class="main_body">
+    
+    <br v-for="n in 10" :key="n">
+  
+     <el-table :data="tableData" border :header-cell-class-name="headerCellClassName" :row-class-name="letterRow">
+       <el-table-column prop="date" label="Date" width="180" />
+       <el-table-column prop="name" label="Name" width="180" />
+       <el-table-column prop="address" label="Address" />
+     </el-table>
+  </div>
 
+</div>
 
-<br v-for="n in 100" :key="n">
-   <el-table :header-cell-class-name="headerCellClassName" :data="tableData">
-      <el-table-column   prop="date" label="Date" width="180" />
-      <el-table-column prop="name" label="Name" width="180" />
-      <el-table-column prop="address" label="Address" />
-      <el-table-column prop="address" label="Address" />
-    </el-table>
-  </template>
+</template>
 
 
 <style scoped lang="scss">
-/* @import "@/assets/sass/pages/member-center/_member-center"; */
+@import "@/assets/sass/pages/member-center/_member-center";
 
 ::v-deep .el-table .warning-row {
   background-color: pink;
   color: red;
 }
 
-
 .my-header {
   background: blue !important; 
 }
 
-
 .column{
 color: red;
-
-
 
 }
 .el-table .warning-row {
@@ -85,4 +96,5 @@ color: red;
   --el-table-tr-bg-color: var(--el-color-success-light-9);
 }
 </style>
+
   

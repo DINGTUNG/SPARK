3c9dd771aef6b8b544a315f05e0eb2f45451dd11
MiMi <janicehsu890228@gmail.com>
2023-07-21 19:48:39 +0800

    推麻煩表格給西卡

--- src/views/pages/sponsor/checkout/SponsorCheckoutStep3.vue
+++ src/views/pages/sponsor/checkout/SponsorCheckoutStep3.vue
@@ -1,25 +1,38 @@
 <script setup>
+import MemberDataTable from '@/components/func-items/table/MemberDataTable.vue'
 import SponsorCheckoutCard from '@/components/func-items/cards/SponsorCheckoutCard.vue'
 import CheckoutButtons from '@/components/func-items/buttons/CheckoutButtons.vue'
+
 
 
 </script>
 
 <template>
-  <div>
-
-
-
-    <SponsorCheckoutCard />
-    <CheckoutButtons/>
-
-
+  <div class="sponsor_checkout_container">
     <!-- 撐開頁面之臨時空間 -->
-    <br v-for="n in 100" :key="n">
+    <br v-for="n in 20" :key="n">
+
+    <div class="main_body">
+      <div class="checkout_wrap">
+
+        <div class="member_data_table">
+          <MemberDataTable />
+        </div>
+
+        <div class="overview_wrap">
+          <SponsorCheckoutCard />
+          <CheckoutButtons />
+        </div>
+
+      </div>
+
+      <img :src="'pictures/decorations/illustration/rocket_with_gold_fire.svg'" :class="filter - gold" alt="">
+
+    </div>
+
 
   </div>
 </template>
 
 <style scoped lang="scss">
-// @import '@/assets/sass/pages/sponsor/checkout/sponsor-checkout-step-3';
-</style>
+@import '@/assets/sass/pages/sponsor/checkout/sponsor-checkout-step-3';</style>

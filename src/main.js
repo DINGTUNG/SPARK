74e6dfe3b71b323090c70d4e87e1c40433160cbb
DINGTUNG <jjwelkintung0124@gmail.com>
2023-07-23 12:34:58 +0800

    刪除element ui 在 main.js 的引用 / 新增 register 頁面router / 會員註冊進度

--- src/main.js
+++ src/main.js
@@ -2,16 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-import { register } from 'swiper/element/bundle';
+
 
 const pinia = createPinia()
 const app = createApp(App)
 
-app.use(register);
+
 app.use(router)
 app.use(pinia)
-app.use(ElementPlus)
 app.mount('#app')
 

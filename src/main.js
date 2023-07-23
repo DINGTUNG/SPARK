bb5e4d4d90f3257232ba64753d7077a2d1e99167
lockon1248 <tom5678tom5678t@yahoo.com.tw>
2023-07-23 18:38:26 +0800

    重推google登入

--- src/main.js
+++ src/main.js
@@ -7,34 +7,16 @@ import Vue3Marquee from 'vue3-marquee'
 import { VueFire, VueFireAuth } from 'vuefire' //引入viewFire
 import { firebaseApp } from './firebase' //引入viewFire
 
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
 
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-import * as labsComponents from 'vuetify/labs/components'
-
-const vuetify = createVuetify({
-  directives,
-  components: {
-    ...components,
-    ...labsComponents,
-  },
-})
 
 
 const pinia = createPinia()
 const app = createApp(App)
-  
 
 
 app.use(Vue3Marquee)
 app.use(router)
 app.use(pinia)
-app.use(vuetify)
-app.mount('#app') 
-
-
 
 //使用viewfire
 app.use(VueFire, {

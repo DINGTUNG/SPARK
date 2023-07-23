8ea84dfca426e36c5edbfc6632d07c9d9bf08312
yiiijie <qaq2104602@gmail.com>
2023-07-23 16:59:27 +0800

    導入vuetify套件

--- src/main.js
+++ src/main.js
@@ -4,15 +4,20 @@ import router from './router'
 import { createPinia } from 'pinia'
 import 'animate.css'
 import Vue3Marquee from 'vue3-marquee'
+import { createVuetify } from 'vuetify'
 
 
 
 const pinia = createPinia()
 const app = createApp(App)
+const vuetify = createVuetify()
+  
 
 
 app.use(Vue3Marquee)
 app.use(router)
 app.use(pinia)
-app.mount('#app')
+app.mount('#app') 
+app.use(vuetify)
+
 

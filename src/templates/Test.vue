b8f515bdddf7ad0ff38bc8d4761cd73230eea145
njs-kayla <kayla881221@gmail.com>
2023-07-24 21:20:07 +0800

    新增後台測試側邊欄(尚未完成)

--- src/templates/Test.vue
+++ src/templates/Test.vue
@@ -1,5 +1,5 @@
 <script lang="js" setup>
-
+import SideBar from '../views/pages/spark-back/SideBar.vue';
 
 </script>
 
@@ -10,7 +10,7 @@
 
       <br v-for="n in 10" :key="n">
 
-    
+      <SideBar/>
     </div>
 
   </div>

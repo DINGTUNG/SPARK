07b2f1402c3db70daf076799eae86b479e88d784
MiMi <janicehsu890228@gmail.com>
2023-07-26 12:34:38 +0800

    調整星火活動RWD，新增跳轉捐助頁面功能

--- src/router/index.js
+++ src/router/index.js
@@ -10,12 +10,12 @@ const router = createRouter({
   history: createWebHistory(
     import.meta.env.BASE_URL),
   // eslint-disable-next-lin2le no-unused-vars
-  scrollBehavior(to, from, savedPosition) {
-    // always scroll to top
-    return {
-      top: 0
-    }
-  },
+  // scrollBehavior(to, from, savedPosition) {
+  //   // always scroll to top
+  //   return {
+  //     top: 0
+  //   }
+  // },
   routes: [{
     path: '/',
     name: 'home',

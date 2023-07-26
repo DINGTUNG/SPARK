aba822df2740b39dd446b0ed61980def2332cb44
MiMi <janicehsu890228@gmail.com>
2023-07-26 19:32:48 +0800

    修改部分圖片路徑

--- src/views/pages/contact/Contact.vue
+++ src/views/pages/contact/Contact.vue
@@ -99,7 +99,7 @@ const getMapUrl = (latitude, longitude) => {
           </iframe>
         </div>
       </div>
-      <img :src="'public/pictures/characters/star/star_superman.svg'" alt="星星超人" class="superman">
+      <img :src="'pictures/characters/star/star_superman.svg'" alt="星星超人" class="superman">
       <img :src="'public/pictures/decorations/illustration/contact_star.svg'" alt="碎星" class="contact_star">
     </div>
   </div>

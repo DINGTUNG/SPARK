cd7e9b31ac9e0b63ac8397a7ac2b9bb34b5453df
njs-kayla <kayla881221@gmail.com>
2023-07-27 18:30:22 +0800

    新增修改會員資料頁面 & 手機板按鈕 & 會員中心改為巢狀路由

--- src/views/pages/member-center/ModifyMeminfo.vue
+++ src/views/pages/member-center/ModifyMeminfo.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { reactive } from 'vue';
-import { useRoute } from 'vue-router';
+
 const Years = reactive([
     2005, '請選擇年份', 1940, 1941, 1942, 1943, 1944, 1945, 1946, 1947, 1948, 1949, 1950, 1951, 1952, 1953, 1954, 1955, 1956, 1957, 1958, 1959, 1960, 1961, 1962, 1963, 1964, 1965, 1966, 1967, 1968, 1969, 1970, 1971, 1972, 1973, 1974, 1975, 1976, 1977, 1978, 1979, 1980, 1981, 1982, 1983, 1984, 1985, 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023
 ])
@@ -11,23 +11,10 @@ const Days = reactive([
     19, '請選擇日期', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31
 ])
 
-const route = useRoute();
-const getButtonStyle = (routePath) => {
-      return {
-        backgroundColor: route.path === routePath ? 'red' : 'blue',
-
-      };
-};
 </script>
 <template>
     <div class="container">
         <div class="main_body">
-            <div class="navigation_button_MB">
-                <router-link :style="getButtonStyle('/adoption-record')" to="/adoption-record">認養紀錄</router-link>
-                <router-link :style="getButtonStyle('/donate-record')" to="/donate-record">捐款紀錄</router-link>
-                <router-link :style="getButtonStyle('/letter-record')" to="/letter-record">感謝函專區</router-link>
-                <router-link :style="getButtonStyle('/modify-meminfo')" to="/modify-meminfo">修改會員資料</router-link>
-            </div>
             <div class="main_form">
             <div class="title">
                 <h1>修改基本資料</h1>

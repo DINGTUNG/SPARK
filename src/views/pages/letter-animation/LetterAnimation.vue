22cc6c6de394758b56b8d6b1953fc733524d76d4
yiiijie <qaq2104602@gmail.com>
2023-08-04 15:15:13 +0800

    新增 修改會員基本資料 上傳大頭照功能（進行中）

--- src/views/pages/letter-animation/LetterAnimation.vue
+++ src/views/pages/letter-animation/LetterAnimation.vue
@@ -17,9 +17,9 @@ const onCloseIconClick = () => {
 <template>
     <div class="bg-wrapper">
         <div class="letter-wrapper" :class="{ flap: isFlap }" ref="letterRef">
-            <span class="text">小朋友來信囉，點擊星星打開看看吧！
+            <span class="text">小朋友來信囉～點擊星星打開看看吧！
             </span>
-            <span class="notice">* 詳細內容請至【會員中心】>【感謝函中心】查看</span>
+            <span class="notice">* 詳細內容請至【會員中心】>【感謝函專區】查看</span>
             <div class="letter animate__bounceIn">
                 <div class="card">
                     <img class="child_letter" :src="'pictures/images/letter-animation/child_letter.png'" alt="感謝函">

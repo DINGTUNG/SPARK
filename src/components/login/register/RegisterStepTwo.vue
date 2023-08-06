7df2e329b138f31d1a95c6bb5ca55893f3ece6ff
DINGTUNG <jjwelkintung0124@gmail.com>
2023-08-06 15:25:46 +0800

    註冊會員第二步驗證進行中

--- src/components/login/register/RegisterStepTwo.vue
+++ src/components/login/register/RegisterStepTwo.vue
@@ -3,6 +3,15 @@ import { ref } from 'vue';
 import { reactive } from "vue";
 import { RouterView } from 'vue-router'
 
+const name = ref('');
+const id_number = ref('');
+const cellphone = ref('');
+const local_phone = ref('');
+const work_phone = ref('');
+const address = ref('');
+
+
+
 
 const Years = reactive([
     '年份', 1940, 1941, 1942, 1943, 1944, 1945, 1946, 1947, 1948, 1949, 1950, 1951, 1952, 1953, 1954, 1955, 1956, 1957, 1958, 1959, 1960, 1961, 1962, 1963, 1964, 1965, 1966, 1967, 1968, 1969, 1970, 1971, 1972, 1973, 1974, 1975, 1976, 1977, 1978, 1979, 1980, 1981, 1982, 1983, 1984, 1985, 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023
@@ -13,6 +22,11 @@ const Months = reactive([
 const Days = reactive([
     '日期', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31
 ])
+
+const submit = () => {
+
+}
+
 
 </script>
 
@@ -146,7 +160,12 @@ const Days = reactive([
                 </div>
             </label>
         </div>
-        <button type="submit" class="submit">送出</button>
+
+        <div v-if="errorContent" class="error_content">
+            {{ errorContent }}
+        </div>
+
+        <button type="submit" class="submit" @click="submit">送出</button>
     </div>
 </template>
 

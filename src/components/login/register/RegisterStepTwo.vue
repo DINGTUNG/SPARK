4d36c93149972970320d5cf483249c8159e852eb
njs-kayla <kayla881221@gmail.com>
2023-08-15 14:06:40 +0800

    刪除不需要的程式碼

--- src/components/login/register/RegisterStepTwo.vue
+++ src/components/login/register/RegisterStepTwo.vue
@@ -26,16 +26,6 @@ const Months = reactive([
 const Days = reactive([
     '日期', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31
 ])
-
-// function showIDNumber() {
-//     document.getElementById("id_number_row").style.display = "block";
-//     document.getElementById("company_number_row").style.display = "none";
-// }
-
-// function showCompanyNumber() {
-//     document.getElementById("id_number_row").style.display = "none";
-//     document.getElementById("company_number_row").style.display = "block";
-// }
 const companyNumber = ref(false);
 const showCompanyNumber = () => {
     companyNumber.value = true;

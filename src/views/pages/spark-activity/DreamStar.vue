4ed9a39a0505822a7ca6d2f1321b3a63e7323df3
DINGTUNG <jjwelkintung0124@gmail.com>
2023-08-16 17:50:20 +0800

    夢想之星單一內容頁 新增支持夢想連結

--- src/views/pages/spark-activity/DreamStar.vue
+++ src/views/pages/spark-activity/DreamStar.vue
@@ -1,8 +1,12 @@
 <script setup>
 import { reactive } from 'vue';
-import { RouterLink } from 'vue-router'
+import { RouterLink } from 'vue-router';
+import { useRouter } from 'vue-router';
 import { useDreamStarStore } from '@/stores/dream-star.js';
+import { useDonateCartStore, DonateProject } from '@/stores/donate-cart.js';
+
 const dreamStarStore = useDreamStarStore()
+const donateCartStore = useDonateCartStore();
 
 const slideList = reactive([{
   imgSrc: 'pictures/images/spark-activity/dream-star/slide_pic_1.png',
@@ -18,8 +22,28 @@ const slideList = reactive([{
 },
 ])
 
+const router = useRouter()
+const goToDonatePageAndScrollToBottom = () => {
+  router.push({ path: '/donate' });
+  // setTimeout(() => {
+  //   window.scrollTo({
+  //     top: 2900,
+  //     left: 0,
+  //     behavior: "smooth",
+  //   })
+  // }, 0)
 
+  donateCartStore.showSideListForActivityDonate();
+  donateCartStore.chosenDonateProject = reactive(DonateProject.TYPE.SPARK_ACTIVITY)
+  scrollToElement();
+}
 
+const scrollToElement = () => {
+  setTimeout(() => {
+    const D007 = document.getElementById(DonateProject.TYPES[6].id)
+    D007.scrollIntoView({ behavior: 'smooth' })
+  }, 500)
+}
 
 
 
@@ -110,7 +134,7 @@ const slideList = reactive([{
           <RouterLink to="/spark-activity">
             <button class="button_start">夢想起點</button>
           </RouterLink>
-          <button class="button_support">點我支持夢想</button>
+          <button class="button_support" @click="goToDonatePageAndScrollToBottom">點我支持夢想</button>
 
         </div>
       </div>
